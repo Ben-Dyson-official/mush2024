87ff18597c2b78c1c9fde5a829ba4e114dca034d
p38360jk <james.keywood@student.manchester.ac.uk>
2024-04-14 00:08:45 +0100

    Outline clusters

--- flask/util.py
+++ flask/util.py
@@ -30,29 +30,12 @@ def classify(filename):
 	thresh = cv2.erode(thresh, None, iterations=0)
 	thresh = cv2.dilate(thresh, None, iterations=1)
 
-	# perform a connected component analysis on the thresholded
-	# image, then initialize a mask to store only the "large"
-	# components
-	labels = measure.label(thresh, background=0)
-	mask = np.zeros(thresh.shape, dtype="uint8")
-	# loop over the unique components
-	for label in np.unique(labels):
-		# if this is the background label, ignore it
-		if label == 0:
-			continue
-		# otherwise, construct the label mask and count the
-		# number of pixels 
-		labelMask = np.zeros(thresh.shape, dtype="uint8")
-		labelMask[labels == label] = 255
-		numPixels = cv2.countNonZero(labelMask)
-		# if the number of pixels in the component is sufficiently
-		# large, then add it to our mask of "large blobs"
-		if numPixels > 1:
-			mask = cv2.add(mask, labelMask)
-
+	star_mask = get_star_mask(thresh)
+	cluster_mask = get_cluster_mask(original_image)
+	
 	# find the contours in the mask, then sort them from left to
 	# right
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cv2.findContours(star_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	cnts = contours.sort_contours(cnts)[0]
 	# loop over the contours
@@ -62,6 +45,13 @@ def classify(filename):
 		((cX, cY), radius) = cv2.minEnclosingCircle(c)
 		cv2.circle(original_image, (int(cX), int(cY)), int(radius), (0, 255, 0), 3)
 		#cv2.putText(image, "#{}".format(i + 1), (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+	# trace the clusters
+	cnts = cv2.findContours(cluster_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	cnts = contours.sort_contours(cnts)[0]
+	for c in cnts:
+		cv2.drawContours(original_image, [c], -1, (255, 255, 0), thickness=10)
 
 	# show the output image
 	cv2.imwrite(os.path.join('./flaskr/static/', filename), original_image)
@@ -140,3 +130,54 @@ def sharpen(img_in, kernal_size):
 	unsharp_mask = cv2.addWeighted(img_in, 1.5, blurred, -0.5, 0)
 
 	return unsharp_mask
+
+def get_star_mask(thresh):
+	# perform a connected component analysis on the thresholded
+	# image, then initialize a mask to store only the "large"
+	# components
+	labels = measure.label(thresh, background=0)
+	mask = np.zeros(thresh.shape, dtype="uint8")
+	
+	# loop over the unique components
+	for label in np.unique(labels):
+		# if this is the background label, ignore it
+		if label == 0:
+			continue
+		# otherwise, construct the label mask and count the
+		# number of pixels 
+		labelMask = np.zeros(thresh.shape, dtype="uint8")
+		labelMask[labels == label] = 255
+		numPixels = cv2.countNonZero(labelMask)
+		# if the number of pixels in the component is sufficiently
+		# large, then add it to our mask of "large blobs"
+		if numPixels > 1 and numPixels < 1000:
+			mask = cv2.add(mask, labelMask)
+
+	return mask
+
+# CREATE MASK FOR BIG CLUSTERS
+def get_cluster_mask(img):
+	lower_val = np.array([0,0,0])
+	upper_val = np.array([40,40,100])
+
+	mask = cv2.inRange(img, lower_val, upper_val)
+
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	selected_contours = []
+
+	for contour in contours:
+		area = cv2.contourArea(contour)
+    
+		if area > 1000: #this is the val we can change
+			selected_contours.append(contour)
+			blank_image = np.zeros(mask.shape, np.uint8)
+			cv2.fillPoly(blank_image, pts=selected_contours, color=(255, 255, 255))
+
+	out_img = cv2.bitwise_not(blank_image)
+	
+	return out_img
+
+
+
+
+
