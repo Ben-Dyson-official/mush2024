ca9d2261eb1a7ba6ea53abda728abde4a125fa81
Ben Dyson <benj.dyson@gmail.com>
2024-04-13 19:09:09 +0100

    Completed the model - pushing to train on windows device

---
+++ AIModel.py
@@ -0,0 +1,64 @@
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+# Standardise image width and height
+img_width = 256
+img_height = 256
+
+batch_size = 16
+
+TRAINING_DIR = '/Users/bendyson/Coding/gitRepos/mush2024/constellation_data/train/'
+
+# Rotate, rescale and zoom to create a wider range of images from different angle
+train_datagen = ImageDataGenerator(rescale=1/255.0, rotation_range=30, zoom_range=0.4)
+
+
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR, batch_size=batch_size, class_mode='categorical', target_size=(img_height, img_width))
+
+# Same for validation
+VALIDATION_DIR = '/Users/bendyson/Coding/gitRepos/mush2024/constellation_data/validate/'
+
+validation_datagen = ImageDataGenerator(rescale=1/255.0, rotation_range=30, zoom_range=0.4)
+
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, batch_size=batch_size, class_mode='categorical', target_size=(img_height, img_width))
+
+callbacks = EarlyStopping(moniter='val_loss', patience=5, verbose=1, mode='auto')
+best_model_file = '/Users/bendyson/Coding/gitRepos/mush2024/CNN_best_weights.h5'
+best_model = ModelCheckpoint(best_model_file, moniter='val_acc', verbose=1, save_best_onoly=True)
+
+# Instantiate the model
+# TODO: Test this and should produce expected outcome
+model = Sequential([
+    Conv2D(16, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)), MaxPooling2D(2, 2),
+    Conv2D(32, (3, 3), activation='relu'), MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    Conv2D(64, (3, 3), activation='relu'), MaxPooling2D(2, 2),
+    Conv2D(128, (3, 3), activation='relu'),
+    Conv2D(128, (3, 3), activation='relu'), MaxPooling2D(2, 2),
+    Conv2D(256, (3, 3), activation='relu'),
+    Conv2D(256, (3, 3), activation='relu'),
+    Conv2D(256, (3, 3), activation='relu'), MaxPooling2D(2, 2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(512, activation='relu'),
+    Dense(16, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=[ 'accuracy'])
+
+# Epochs should be increased to fully train (but will take longer)
+history = model.fit_generator(train_generator, epochs=30, verbose=1, validation_data=validation_generator, callbacks=[best_model])
+
+
+
+
+
+
+
+
