57493095a019fad83c1e9858fce98ddcccc342c4
jameskeywood <jskeywood@gmail.com>
2024-04-13 21:13:33 +0100

    Train model on 20 epochs

--- AIModel.py
+++ AIModel.py
@@ -1,3 +1,7 @@
+import os
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.0/bin")
+os.add_dll_directory("C:/Users/James/Documents/cuda/bin")
+
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
@@ -11,7 +15,7 @@ img_height = 256
 
 batch_size = 16
 
-TRAINING_DIR = '/Users/bendyson/Coding/gitRepos/mush2024/constellation_data/train/'
+TRAINING_DIR = "C:/Users/James/Desktop/mush2024/constellation_data/train"
 
 # Rotate, rescale and zoom to create a wider range of images from different angle
 train_datagen = ImageDataGenerator(rescale=1/255.0, rotation_range=30, zoom_range=0.4)
@@ -20,15 +24,16 @@ train_datagen = ImageDataGenerator(rescale=1/255.0, rotation_range=30, zoom_rang
 train_generator = train_datagen.flow_from_directory(TRAINING_DIR, batch_size=batch_size, class_mode='categorical', target_size=(img_height, img_width))
 
 # Same for validation
-VALIDATION_DIR = '/Users/bendyson/Coding/gitRepos/mush2024/constellation_data/validate/'
+VALIDATION_DIR = "C:/Users/James/Desktop/mush2024/constellation_data/validation"
 
 validation_datagen = ImageDataGenerator(rescale=1/255.0, rotation_range=30, zoom_range=0.4)
 
 validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, batch_size=batch_size, class_mode='categorical', target_size=(img_height, img_width))
 
-callbacks = EarlyStopping(moniter='val_loss', patience=5, verbose=1, mode='auto')
-best_model_file = '/Users/bendyson/Coding/gitRepos/mush2024/CNN_best_weights.h5'
-best_model = ModelCheckpoint(best_model_file, moniter='val_acc', verbose=1, save_best_onoly=True)
+
+callbacks = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto')
+best_model_file = 'C:/Users/James/Desktop/mush2024/CNN_best_weights.h5'
+best_model = ModelCheckpoint(best_model_file, monitor='val_acc', verbose=1, save_best_onoly=True)
 
 # Instantiate the model
 # TODO: Test this and should produce expected outcome
@@ -53,10 +58,7 @@ model.summary()
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=[ 'accuracy'])
 
 # Epochs should be increased to fully train (but will take longer)
-history = model.fit_generator(train_generator, epochs=30, verbose=1, validation_data=validation_generator, callbacks=[best_model])
-
-
-
+history = model.fit_generator(train_generator, epochs=20, verbose=1, validation_data=validation_generator, callbacks=[best_model])
 
 
 
