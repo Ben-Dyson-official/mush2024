390c6a00f44fce68ccb196e7a0f98573566ffeb7
harrrrion <harrisonrigby@icloud.com>
2024-04-14 01:24:22 +0100

    csv stuff

--- flask/flaskr/home.py
+++ flask/flaskr/home.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, render_template, redirect, request, flash
 import os
-
+import random
+import csv
 import util
 
 bp = Blueprint("home", __name__, url_prefix="/")
 
 # routes
+
+def read_csv(path):
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        facts = list(reader)
+
+        random_fact = random.choice(facts[1:])
+
+    return random_fact
 
 @bp.route("")
 def handle_empty():
@@ -17,7 +27,9 @@ def home_page():
 
 @bp.route("home", methods=['POST'])
 def image_submit():
-	file = request.files['inputFile']
-	file.save(os.path.join('./flaskr/static/', file.filename))
-	util.classify(file.filename)
-	return render_template("home/home.html", filename=file.filename)
+    file = request.files['inputFile']
+    file.save(os.path.join('./flaskr/static/', file.filename))
+    util.classify(file.filename)
+    fact = read_csv('../constellation_facts.csv')
+
+    return render_template("home/home.html", filename=file.filename, fact=fact)
